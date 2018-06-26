535aa0ea0672426e53f2055f3269c21a7ec2feb7
Puit <40431164+Puit@users.noreply.github.com>
2018-06-26 12:18:32 +0200

    XUnit Test

---
+++ AlumnoXunit.Integration.Test/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AlumnoXunit.Integration.Test
+{
+     public class Class1
+    {
+        private  miAlumno = new PooVueling.Calculadora();
+        [Fact]
+        public void SumaTest()
+        {
+            var result = miCalculadora.Division(4, 2);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void SumaFailingTest()
+        {
+            var result = miCalculadora.Division(4, 2);
+            Assert.Equal(5, result);
+        }
+    }
+}
---
+++ PooVueling_AlumnoXunit.Integration.test/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+//namespace AlumnoXUnitTest
+namespace PooVueling_Alumno
+{
+    public class Class1
+    {
+        private Alumno alumnoTest = new Alumno();
+        [Fact]
+        public void PassingTest()
+        {
+            alumnoTest.Nombre = "Abcde";
+            alumnoTest.Apellidos = "Fghi Jqlm";
+            alumnoTest.DNI = "11111111v";
+            alumnoTest.Id = alumnoTest.GetHashCode().ToString();
+            bool result = alumnoTest.Guardar();
+            Assert.True(result);
+
+        }
+
+        //[Fact]
+        //public void FailingTest()
+        //{
+        //    Assert.Equal(5, Add(2, 2));
+        //}
+    }
+}
